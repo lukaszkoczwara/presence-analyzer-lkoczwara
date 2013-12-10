f3b84b7f9f1d90b9cd19d887146c55a957e6feb1
LukaszKoczwara <lukasz.koczwara@stxnext.pl>
2013-12-10 13:07:23 +0100

    refs #893 add buildout files

--- src/presence_analyzer/__init__.py
+++ src/presence_analyzer/__init__.py
@@ -2,15 +2,16 @@
 """
 Presence analyzer web app.
 """
+import importlib
 import os.path
 import logging.config
 
-from presence_analyzer.main import app
-import presence_analyzer.views
+from .main import app
+from . import views
 
 
-if __name__ == "__main__":
-    ini_filename = os.path.join(os.path.dirname(__file__),
-                                '..', '..', 'runtime', 'debug.ini')
-    logging.config.fileConfig(ini_filename, disable_existing_loggers=False)
-    app.run(host='0.0.0.0')
+# if __name__ == "__main__":
+#     ini_filename = os.path.join(os.path.dirname(__file__),
+#                                 '..', '..', 'runtime', 'debug.ini')
+#     logging.config.fileConfig(ini_filename, disable_existing_loggers=False)
+#     app.run(host='0.0.0.0')
