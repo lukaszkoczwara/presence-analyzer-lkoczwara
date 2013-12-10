59abe408c1f04d325991695a4a723ba3a6e30ebf
LukaszKoczwara <lukasz.koczwara@stxnext.pl>
2013-12-10 15:03:21 +0100

    refs #893 cleanup

--- src/presence_analyzer/__init__.py
+++ src/presence_analyzer/__init__.py
@@ -2,16 +2,6 @@
 """
 Presence analyzer web app.
 """
-import importlib
-import os.path
-import logging.config
-
 from .main import app
 from . import views
 
-
-# if __name__ == "__main__":
-#     ini_filename = os.path.join(os.path.dirname(__file__),
-#                                 '..', '..', 'runtime', 'debug.ini')
-#     logging.config.fileConfig(ini_filename, disable_existing_loggers=False)
-#     app.run(host='0.0.0.0')
